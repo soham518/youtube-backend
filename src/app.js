8f079914afb47ff022ef42c9b022aa880a729fa5
soham518 <167617833+soham518@users.noreply.github.com>
2025-07-08 13:59:32 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -13,4 +13,12 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+//routes import
+import userRouter from "./routes/user.routes.js"
+
+//routes decleration
+
+app.use("/api/v1/users", userRouter)
+//http:localhost:8000/api/vi/users/register
 export { app };
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,12 +1,20 @@
-// const asyncHandler = () => {}
-const asyncHandler = (fn) => async (req, res, next) => {
-  try {
-    await fn(req, res, next);
-  } catch (error) {
-    req.status(err.code || 500).json({
-      success: false,
-      message: err.message,
-    });
+
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req,res,next)).catch
+    ((err)=> next(err))
   }
-};
+}
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     req.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
 export { asyncHandler };
